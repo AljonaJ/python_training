2a9e3d810acd9b9a50a5474ba82bce5876a6d056
AljonaJ <74924714+AljonaJ@users.noreply.github.com>
2020-11-25 22:00:45 +0200

    homework1

---
+++ lesson2.py
@@ -0,0 +1,26 @@
+# a, b = input('Please enter two numbers: ').split(' ')
+# try:
+#     print(int(a) / int(b))
+# except:
+#     print('error')
+
+# a = 0
+# while a == 0:
+#     print('Hello')
+#     break
+
+import math  # from math import sqrt (импортируется часть библиотеки), import math as mt (переим. в удобное название)
+
+a = 107.332
+b = math.ceil(a)  # округление в большую сторону -> print(math.ceil(a))
+print(b)
+
+c = math.floor(a)  # округление в меньшую сторону
+print(c)
+
+number = [1, 2, 3, 4, 4.3]
+count = sum(number)
+print(count)
+
+z = 25
+print(math.sqrt(z))
