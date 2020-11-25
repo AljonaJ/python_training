2e151978f065d51d3a03e2f8d5efa19bcc27aab2
AljonaJ <74924714+AljonaJ@users.noreply.github.com>
2020-11-26 01:00:19 +0200

    homework 2

---
+++ homework_2.py
@@ -0,0 +1,24 @@
+try:
+    personal_code = str(int(input('Please enter your personal code\n')))
+
+    if len(personal_code) != 11:
+        print('Personal code length is wrong')
+        exit()
+
+    if personal_code[0] == '2' or personal_code[0] == '4' or personal_code[0] == '6':
+        print(
+            'You are woman. Your birthday is ',
+            personal_code[5:7], '.', personal_code[3:5], '.', personal_code[1:3],
+            sep=''
+        )
+    elif personal_code[0] == '1' or personal_code[0] == '3' or personal_code[0] == '5':
+            print(
+                'You are man. Your birthday is ',
+                personal_code[5:7], '.', personal_code[3:5], '.', personal_code[1:3],
+                sep=''
+            )
+    else:
+        print('Something is wrong. Please check your personal code.')
+
+except ValueError:
+    print('Entered personal code consists letters or symbols')
--- lesson2.py
+++ lesson2.py
@@ -1,6 +1,6 @@
-# a, b = input('Please enter two numbers: ').split(' ')
+a, b = input('Please enter two numbers: ').split(' ')
 # try:
-#     print(int(a) / int(b))
+print(int(a) / int(b))
 # except:
 #     print('error')
 
@@ -9,18 +9,18 @@
 #     print('Hello')
 #     break
 
-import math  # from math import sqrt (импортируется часть библиотеки), import math as mt (переим. в удобное название)
-
-a = 107.332
-b = math.ceil(a)  # округление в большую сторону -> print(math.ceil(a))
-print(b)
-
-c = math.floor(a)  # округление в меньшую сторону
-print(c)
-
-number = [1, 2, 3, 4, 4.3]
-count = sum(number)  # сумма чисел из списка
-print(count)
-
-z = 25
-print(math.sqrt(z))
+# import math  # from math import sqrt (импортируется часть библиотеки), import math as mt (переим. в удобное название)
+#
+# a = 107.332
+# b = math.ceil(a)  # округление в большую сторону -> print(math.ceil(a))
+# print(b)
+#
+# c = math.floor(a)  # округление в меньшую сторону
+# print(c)
+#
+# number = [1, 2, 3, 4, 4.3]
+# count = sum(number)  # сумма чисел из списка
+# print(count)
+#
+# z = 25
+# print(math.sqrt(z))
