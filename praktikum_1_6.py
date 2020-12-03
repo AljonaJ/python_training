912e216887918446979ae13b65ffae20ca0d0471
AljonaJ <74924714+AljonaJ@users.noreply.github.com>
2020-12-03 14:33:26 +0200

    praktikum 1_6

--- praktikum_1_6.py
+++ praktikum_1_6.py
@@ -1,1 +1,7 @@
-list_example = input('Enter random numbers: ')
+list_example = input('Enter random numbers: ').split()
+new_list = []
+
+for num in list_example:
+    if list_example.count(num) > 1 and num not in new_list:
+        new_list.append(num)
+print(new_list)
